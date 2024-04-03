316572471e2d03c0a41781a6f52a9984f94b7acb
Reynaldev <reynaldy.nur.cahyo.003@gmail.com>
2024-04-03 18:08:44 +0700

    Cell drawings

--- ImpossibleTicTacToe/main.cpp
+++ ImpossibleTicTacToe/main.cpp
@@ -1,45 +1,104 @@
 #include "main.h"
 #include "crossshape.h"
 
-struct Symbol
+struct Cell
 {
-	union
-	{
-		CrossShape crossShape;
-		sf::CircleShape circleShape;
-	};
+	sf::RectangleShape shape;
 
 	SymbolTypeFlag type;
-	float x, y;
+	float x, y, w, h;
 
-	void createCross(float posX, float posY)
+	Cell() {}
+
+	Cell(sf::Vector2f pos, sf::Vector2f size)
 	{
-		this->x = posX;
-		this->y = posY;
-		type = SYMBOL_TYPE_CROSS;
+		x = pos.x;
+		y = pos.y;
+		w = size.x;
+		h = size.y;
 
-		crossShape = CrossShape(sf::Vector2f(x, y));
+		shape.setSize(size);
+		shape.setPosition(pos);
+		shape.setFillColor(sf::Color::White);
+
+		printf("X: %.1f\tY: %.1f\n", x, y);
+		printf("Shape X: %.1f\tShape Y: %.1f\n", shape.getPosition().x, shape.getPosition().y);
+		printf("W: %.1f\tH: %.1f\n", w, h);
+		printf("Shape W: %.1f\tShape H: %.1f\n", shape.getSize().x, shape.getSize().y);
 	}
 
-	void createCircle(float radius)
+	void setPosition(sf::Vector2f pos)
 	{
-		type = SYMBOL_TYPE_CIRCLE;
+		x = pos.x;
+		y = pos.y;
 
-		circleShape = sf::CircleShape(radius);
+		shape.move(pos);
 	}
 
-	void draw(sf::RenderWindow &window) const
+	void onMouseEntered(const sf::Event &e)
 	{
-		switch (type)
+		sf::Vector2f mousePos((float)e.mouseMove.x, (float)e.mouseMove.y);
+
+		if (mousePos.x > x
+			&& mousePos.x < (x + w)
+			&& mousePos.y > y
+			&& mousePos.y < (y + h))
 		{
-		case SYMBOL_TYPE_CROSS:
-			crossShape.draw(window);
-			break;
-		case SYMBOL_TYPE_CIRCLE:
-			window.draw(circleShape);
-			break;
+			shape.setOutlineThickness(5.f);
+			shape.setOutlineColor(sf::Color::Blue);
 		}
+
+		//printf("Mouse:\n\tX: %.1f\tY: %.1f\n", mousePos.x, mousePos.y);
 	}
+
+	void onMouseButtonClicked(const sf::Event &e)
+	{
+		sf::Vector2f mousePos((float)e.mouseButton.x, (float)e.mouseButton.y);
+
+		if (mousePos.x > x
+			&& mousePos.x < (x + w)
+			&& mousePos.y > y
+			&& mousePos.y < (y + h))
+		{
+			printf("Square at X: %.1f Y: %.1f\n", x, y);
+			shape.setFillColor(sf::Color(50, 50, 50));
+		}
+
+	}
+
+	void draw()
+	{
+
+	}
+
+	//void createCross(float posX, float posY)
+	//{
+	//	this->x = posX;
+	//	this->y = posY;
+	//	type = SYMBOL_TYPE_CROSS;
+
+	//	crossShape = CrossShape(sf::Vector2f(x, y));
+	//}
+
+	//void createCircle(float radius)
+	//{
+	//	type = SYMBOL_TYPE_CIRCLE;
+
+	//	circleShape = sf::CircleShape(radius);
+	//}
+
+	//void draw(sf::RenderWindow &window) const
+	//{
+	//	switch (type)
+	//	{
+	//	case SYMBOL_TYPE_CROSS:
+	//		crossShape.draw(window);
+	//		break;
+	//	case SYMBOL_TYPE_CIRCLE:
+	//		window.draw(circleShape);
+	//		break;
+	//	}
+	//}
 };
 
 int main()
@@ -47,7 +106,21 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Impossible TicTacToe");
 	window.setVerticalSyncEnabled(true);
 
-	CrossShape cross(sf::Vector2f(0.0f, 150.0f));
+	//CrossShape cross(sf::Vector2f(0.0f, 150.0f));
+
+	std::vector<Cell> cells;
+
+	for (int i = 0; i < 9; i++)
+	{
+		float row = (float)(floor(i / 3) * 3);
+		float col = (float)(row + i);
+
+		int randColor = rand() % 256;
+
+		Cell cell(sf::Vector2f(50.f * fmodf(col, 3), 50.f * (row / 3.f)), sf::Vector2f(50.f, 50.f));
+		cell.shape.setFillColor(sf::Color(randColor, randColor, randColor));
+		cells.push_back(cell);
+	}
 
 	while (window.isOpen())
 	{
@@ -56,13 +129,24 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			if (event.type == sf::Event::MouseMoved)
+				for (Cell cell : cells)
+					cell.onMouseEntered(event);
+
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				if (event.mouseButton.button == sf::Mouse::Left)
+					for (Cell cell : cells)
+						cell.onMouseButtonClicked(event);
+			}
 		}
 
 		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 		{
 			sf::Vector2f mousePos = (sf::Vector2f) sf::Mouse::getPosition(window);
 
-			cross.move(mousePos);
+			//cross.move(mousePos);
 
 			//lines[0].position = sf::Vector2f((float)mousePos.x - (150.0f / 2.0f), (float)mousePos.y - (150.0f / 2.0f));
 			//lines[1].position = sf::Vector2f((float)mousePos.x + (150.0f / 2.0f), (float)mousePos.y + (150.0f / 2.0f));
@@ -75,7 +159,12 @@ int main()
 
 		window.clear();
 
-		cross.draw(window);
+		//cross.draw(window);
+
+		for (Cell cell : cells)
+		{
+			window.draw(cell.shape);
+		}
 
 		window.display();
 	}
--- ImpossibleTicTacToe/main.h
+++ ImpossibleTicTacToe/main.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <array>
 
 #include <SFML/Graphics.hpp>
 

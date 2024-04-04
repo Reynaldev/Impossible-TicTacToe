b603dafe0d01012c9455ae1992e65e66050078b4
Reynaldev <reynaldy.nur.cahyo.003@gmail.com>
2024-04-04 16:20:10 +0700

    Added background color and improved cell UI

--- ImpossibleTicTacToe/main.cpp
+++ ImpossibleTicTacToe/main.cpp
@@ -12,14 +12,14 @@ struct Cell
 
 	Cell(sf::Vector2f pos, sf::Vector2f size)
 	{
-		x = pos.x;
-		y = pos.y;
-		w = size.x;
-		h = size.y;
+		x = pos.x + 2.5f;
+		y = pos.y + 2.5f;
+		w = size.x - 2.5f;
+		h = size.y - 2.5f;
 
-		shape.setSize(size);
-		shape.setPosition(pos);
-		shape.setFillColor(sf::Color::White);
+		shape.setSize(sf::Vector2f(w, h));
+		shape.setPosition(sf::Vector2f(x, y));
+		shape.setFillColor(sf::Color(150, 150, 150));
 		shape.setOutlineColor(sf::Color::White);
 
 		//printf("X: %.1f\tY: %.1f\n", x, y);
@@ -64,10 +64,12 @@ struct Cell
 			&& mousePos.y > y
 			&& mousePos.y < (y + h))
 		{
-			printf("Square at X: %.1f Y: %.1f\n", x, y);
 			shape.setFillColor(sf::Color(50, 50, 50));
 		}
-
+		else
+		{
+			shape.setFillColor(sf::Color(150, 150, 150));
+		}
 	}
 
 	void draw()
@@ -110,8 +112,6 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Impossible TicTacToe");
 	window.setVerticalSyncEnabled(true);
 
-	//CrossShape cross(sf::Vector2f(0.0f, 150.0f));
-
 	std::vector<Cell> cells;
 
 	for (int i = 0; i < 9; i++)
@@ -132,7 +132,6 @@ int main()
 			),
 			sf::Vector2f(size, size)
 		);
-		cell.shape.setFillColor(sf::Color(randColor, randColor, randColor));
 		cells.push_back(cell);
 	}
 
@@ -158,22 +157,7 @@ int main()
 			}
 		}
 
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-		{
-			sf::Vector2f mousePos = (sf::Vector2f) sf::Mouse::getPosition(window);
-
-			//cross.move(mousePos);
-
-			//lines[0].position = sf::Vector2f((float)mousePos.x - (150.0f / 2.0f), (float)mousePos.y - (150.0f / 2.0f));
-			//lines[1].position = sf::Vector2f((float)mousePos.x + (150.0f / 2.0f), (float)mousePos.y + (150.0f / 2.0f));
-			//lines[2].position = sf::Vector2f((float)mousePos.x + (150.0f / 2.0f), (float)mousePos.y - (150.0f / 2.0f));
-			//lines[3].position = sf::Vector2f((float)mousePos.x - (150.0f / 2.0f), (float)mousePos.y + (150.0f / 2.0f));
-
-			//line1.setPosition(mousePos.x - (line1.getSize().x / 2.0f), mousePos.y - (line1.getSize().x / 2.0f));
-			//line2.setPosition(mousePos.x - (line2.getSize().x / 2.0f), mousePos.y - (line2.getSize().y / 2.0f));
-		}
-
-		window.clear();
+		window.clear(sf::Color(200, 200, 200));
 
 		//cross.draw(window);
 

15c324b1cc4ed5da60749e980c7bccdd29022e43
Reynaldev <reynaldy.nur.cahyo.003@gmail.com>
2024-04-10 19:08:45 +0700

    Better modal

--- ImpossibleTicTacToe/modal.h
+++ ImpossibleTicTacToe/modal.h
@@ -11,39 +11,46 @@ namespace Modal
 		sf::Font mFont;
 		sf::Text mText;
 		std::string mTitle;
-	};
+	} static mModal;
 
 	static void show(sf::RenderWindow &window, const std::string &title, const std::string &text)
 	{
-		static Modal modal;
+		static Modal *modal;
 
-		modal.modalActive = true;
-
-		if (!modal.mFont.loadFromFile("assets/fonts/Minecraft.ttf"))
+		if (!modal)
 		{
-			printf("Cannot load the font file for modal\n");
-			return;
+			printf("Modal initialized!\n");
+
+			modal = &mModal;
+
+			modal->modalActive = true;
+
+			if (!modal->mFont.loadFromFile("assets/fonts/Minecraft.ttf"))
+			{
+				printf("Cannot load the font file for modal\n");
+				return;
+			}
+
+			modal->mText.setFont(modal->mFont);
+			modal->mText.setString(text);
+			modal->mText.setCharacterSize(18);
+
+			modal->mShape.setSize(sf::Vector2f(250.f, modal->mText.getLocalBounds().height + 10.f));
+			modal->mShape.setPosition(((sf::Vector2f)window.getSize() - modal->mShape.getSize()) / 2.f);
+			modal->mShape.setFillColor(sf::Color(200, 200, 200));
+			modal->mShape.setOutlineThickness(5.f);
+			modal->mShape.setOutlineColor(sf::Color::White);
+
+			modal->mText.setPosition(
+				sf::Vector2f(
+					((modal->mShape.getPosition().x + modal->mShape.getSize().x) / 2.f) + modal->mText.getLocalBounds().width,
+					modal->mShape.getPosition().y
+				)
+			);
 		}
 
-		modal.mText.setFont(modal.mFont);
-		modal.mText.setString(text);
-		modal.mText.setCharacterSize(18);
-
-		modal.mShape.setSize(sf::Vector2f(250.f, modal.mText.getLocalBounds().height + 10.f));
-		modal.mShape.setPosition(((sf::Vector2f)window.getSize() - modal.mShape.getSize()) / 2.f);
-		modal.mShape.setFillColor(sf::Color(200, 200, 200));
-		modal.mShape.setOutlineThickness(5.f);
-		modal.mShape.setOutlineColor(sf::Color::White);
-
-		modal.mText.setPosition(
-			sf::Vector2f(
-				((modal.mShape.getPosition().x + modal.mShape.getSize().x) / 2.f) + modal.mText.getLocalBounds().width,
-				modal.mShape.getPosition().y
-			)
-		);
-
-		window.draw(modal.mShape);
-		window.draw(modal.mText);
+		window.draw(modal->mShape);
+		window.draw(modal->mText);
 	}
 
 };
